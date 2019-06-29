3dc170200f5914a10dd333cefb3a5f3248cd316e
leofun01 <leofun01@gmail.com>
2019-06-30 00:26:34 +0300

    Remove unused private field `index`. Update file `Points.cs`

--- Group.Net/Points.cs
+++ Group.Net/Points.cs
@@ -9,7 +9,6 @@ namespace Group.Net
 	public class Points : IComparable<Points>, IEnumerable<int>
 	{
 		private readonly IList<int> points;
-		private readonly IList<int> index;
 
 		public int Count
 		{
